77fd336c04edb7d823fba29432a80376efe02ab2
yvargas05 <ysabellavargas05@gmail.com>
2024-10-29 18:44:30 -0400

    bog

---
+++ GoThroughTime/GoThroughTime/Assets/yssies stuff/respawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+   public Transform respawnPoint;
+   void OnTriggerEnter(Collider other){
+             if (other.gameObject.tag == "Player") {
+                other.gameObject.transform.position = respawnPoint.position;
+             }
+      }
+}
