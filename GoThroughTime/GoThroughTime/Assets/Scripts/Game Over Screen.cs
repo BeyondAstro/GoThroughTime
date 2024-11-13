01ddaf856a2f637d8621d1876c4380c37b25a468
JuniorTDavis <lintontjoesph@gmail.com>
2024-11-13 00:17:48 -0500

    Menu Changes

--- GoThroughTime/GoThroughTime/Assets/Scripts/Game Over Screen.cs
+++ GoThroughTime/GoThroughTime/Assets/Scripts/Game Over Screen.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Restart()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Main Menu");
     }
 }
