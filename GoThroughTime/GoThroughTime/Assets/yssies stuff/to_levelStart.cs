876f66a7ac077eb517624e170d2a0ef284e7a848
yvargas05 <ysabellavargas05@gmail.com>
2024-11-19 18:58:17 -0500

    fixed some

---
+++ GoThroughTime/GoThroughTime/Assets/yssies stuff/to_levelStart.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class to_levelStart : MonoBehaviour
+{
+    private bool playerNearby = false; 
+    public GameObject direction;
+
+    void Start(){
+        direction.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.A)) // Detect player interaction
+        {
+            SwitchScenes();
+        }
+
+    }
+    private void OnTriggerEnter(Collider book)
+    {
+        if (book.CompareTag("Player")) // Ensure it's the player interacting
+        {
+            Debug.Log("Book touched!");
+            playerNearby = true;
+            direction.gameObject.SetActive(true);
+        }
+    }
+
+    public void SwitchScenes()
+    {
+        SceneManager.LoadScene("yssie");
+    }
+
+}
