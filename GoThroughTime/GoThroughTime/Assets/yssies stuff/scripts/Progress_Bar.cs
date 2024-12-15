0766b1727d3145299facfe95622f74d1d5b3808c
yvargas05 <ysabellavargas05@gmail.com>
2024-12-15 14:30:21 -0500

    progress_bar

---
+++ GoThroughTime/GoThroughTime/Assets/yssies stuff/scripts/Progress_Bar.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour {
+    private GameObject player;
+    public GameObject clock; // The clock UI element
+    public GameObject[] hands; // Array to hold hand UI elements (secondHand, minHand, hourHand)
+
+    public static int gotHands = 0; // Counter for collected hands
+
+    void Start() {
+        player = GameObject.FindWithTag("Player");
+        UpdateStatsDisplay();
+        InitializeHands();
+    }
+
+    // Initialize hands to be inactive
+    private void InitializeHands() {
+        foreach (GameObject hand in hands) {
+            hand.SetActive(false); // Deactivate all hands initially
+        }
+        clock.SetActive(true); // Ensure the clock is active
+    }
+
+    // Update the UI display based on collected hands
+    public void UpdateStatsDisplay() {
+        // Activate the clock and the hands based on the number of collected hands
+        for (int i = 0; i < gotHands; i++) {
+            if (i < hands.Length) {
+                hands[i].SetActive(true); // Activate the hand corresponding to the collected count
+            }
+        }
+    }
+
+    // Call this method when the player collects a hand
+    public void CollectHand() {
+        gotHands++;
+        UpdateStatsDisplay();
+    }
+
+    // Example method to detect collision with collectible hands
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Hand")) {
+            CollectHand();
+            Destroy(other.gameObject); // Destroy the collected hand object
+        }
+    }
+
+}
